cfcc854bd3a8a808d3ef93bf73528f3c4d949528
hamster1979 <hamster1979@yandex.ru>
2018-09-25 11:20:00 +0300

    Added Get, Attr, Create commands to Task Service

--- intraservice.go
+++ intraservice.go
@@ -31,6 +31,109 @@ func (e APIError) Error() string {
 	return fmt.Sprintf("intraservice: %v %v", e.Message, e.MessageDetail)
 }
 
+//CustomFields - custom Fields type for parameters
+type CustomFields struct {
+	Field1  string `url:"Field1,omitempty"`
+	Field2  string `url:"Field2,omitempty"`
+	Field3  string `url:"Field3,omitempty"`
+	Field4  string `url:"Field4,omitempty"`
+	Field5  string `url:"Field5,omitempty"`
+	Field6  string `url:"Field6,omitempty"`
+	Field7  string `url:"Field7,omitempty"`
+	Field8  string `url:"Field8,omitempty"`
+	Field9  string `url:"Field9,omitempty"`
+	Field10 string `url:"Field10,omitempty"`
+	Field11 string `url:"Field11,omitempty"`
+	Field12 string `url:"Field12,omitempty"`
+	Field13 string `url:"Field13,omitempty"`
+	Field14 string `url:"Field14,omitempty"`
+	Field15 string `url:"Field15,omitempty"`
+	Field16 string `url:"Field16,omitempty"`
+	Field17 string `url:"Field17,omitempty"`
+	Field18 string `url:"Field18,omitempty"`
+	Field19 string `url:"Field19,omitempty"`
+	Field20 string `url:"Field20,omitempty"`
+	Field21 string `url:"Field21,omitempty"`
+	Field22 string `url:"Field22,omitempty"`
+	Field23 string `url:"Field23,omitempty"`
+	Field24 string `url:"Field24,omitempty"`
+	Field25 string `url:"Field25,omitempty"`
+	Field26 string `url:"Field26,omitempty"`
+	Field27 string `url:"Field27,omitempty"`
+	Field28 string `url:"Field28,omitempty"`
+	Field29 string `url:"Field29,omitempty"`
+	Field30 string `url:"Field30,omitempty"`
+	Field31 string `url:"Field31,omitempty"`
+	Field32 string `url:"Field32,omitempty"`
+	Field33 string `url:"Field33,omitempty"`
+	Field34 string `url:"Field34,omitempty"`
+	Field35 string `url:"Field35,omitempty"`
+	Field36 string `url:"Field36,omitempty"`
+	Field37 string `url:"Field37,omitempty"`
+	Field38 string `url:"Field38,omitempty"`
+	Field39 string `url:"Field39,omitempty"`
+	Field40 string `url:"Field40,omitempty"`
+	Field41 string `url:"Field41,omitempty"`
+	Field42 string `url:"Field42,omitempty"`
+	Field43 string `url:"Field43,omitempty"`
+	Field44 string `url:"Field44,omitempty"`
+	Field45 string `url:"Field45,omitempty"`
+	Field46 string `url:"Field46,omitempty"`
+	Field47 string `url:"Field47,omitempty"`
+	Field48 string `url:"Field48,omitempty"`
+	Field49 string `url:"Field49,omitempty"`
+	Field50 string `url:"Field50,omitempty"`
+	Field51 string `url:"Field51,omitempty"`
+	Field52 string `url:"Field52,omitempty"`
+	Field53 string `url:"Field53,omitempty"`
+	Field54 string `url:"Field54,omitempty"`
+	Field55 string `url:"Field55,omitempty"`
+	Field56 string `url:"Field56,omitempty"`
+	Field57 string `url:"Field57,omitempty"`
+	Field58 string `url:"Field58,omitempty"`
+	Field59 string `url:"Field59,omitempty"`
+	Field60 string `url:"Field60,omitempty"`
+	Field61 string `url:"Field61,omitempty"`
+	Field62 string `url:"Field62,omitempty"`
+	Field63 string `url:"Field63,omitempty"`
+	Field64 string `url:"Field64,omitempty"`
+	Field65 string `url:"Field65,omitempty"`
+	Field66 string `url:"Field66,omitempty"`
+	Field67 string `url:"Field67,omitempty"`
+	Field68 string `url:"Field68,omitempty"`
+	Field69 string `url:"Field69,omitempty"`
+	Field70 string `url:"Field70,omitempty"`
+	Field71 string `url:"Field71,omitempty"`
+	Field72 string `url:"Field72,omitempty"`
+	Field73 string `url:"Field73,omitempty"`
+	Field74 string `url:"Field74,omitempty"`
+	Field75 string `url:"Field75,omitempty"`
+	Field76 string `url:"Field76,omitempty"`
+	Field77 string `url:"Field77,omitempty"`
+	Field78 string `url:"Field78,omitempty"`
+	Field79 string `url:"Field79,omitempty"`
+	Field80 string `url:"Field80,omitempty"`
+	Field81 string `url:"Field81,omitempty"`
+	Field82 string `url:"Field82,omitempty"`
+	Field83 string `url:"Field83,omitempty"`
+	Field84 string `url:"Field84,omitempty"`
+	Field85 string `url:"Field85,omitempty"`
+	Field86 string `url:"Field86,omitempty"`
+	Field87 string `url:"Field87,omitempty"`
+	Field88 string `url:"Field88,omitempty"`
+	Field89 string `url:"Field89,omitempty"`
+	Field90 string `url:"Field90,omitempty"`
+	Field91 string `url:"Field91,omitempty"`
+	Field92 string `url:"Field92,omitempty"`
+	Field93 string `url:"Field93,omitempty"`
+	Field94 string `url:"Field94,omitempty"`
+	Field95 string `url:"Field95,omitempty"`
+	Field96 string `url:"Field96,omitempty"`
+	Field97 string `url:"Field97,omitempty"`
+	Field98 string `url:"Field98,omitempty"`
+	Field99 string `url:"Field99,omitempty"`
+}
+
 //FilterFields - are the parameters for the fields filtering
 type FilterFields struct {
 	ChangedLessThan      string `url:"ChangedLessThan,omitempty"`
@@ -125,54 +228,149 @@ type Task struct {
 	Observers                    string `json:"Observers,omitempty"`
 	ObserverIds                  string `json:"ObserverIds,omitempty"`
 	Data                         string `json:"Data,omitempty"`
-	CompletionStatus             int    `json:"CompletionStatus,omitempty"`
-	WorkflowID                   int    `json:"WorkflowId,omitempty"`
-	IsClient                     bool   `json:"IsClient,omitempty"`
-	FieryFields                  string `json:"FieryFields,omitempty"`
-	IsConcurrence                bool   `json:"IsConcurrence,omitempty"`
-	IsCoordinatedByCurrentUser   bool   `json:"IsCoordinatedByCurrentUser,omitempty"`
-	CreatorADGuid                string `json:"CreatorADGuid,omitempty"`
-	CreatorComments              string `json:"CreatorComments,omitempty"`
-	CreatorCompanyID             int    `json:"CreatorCompanyId,omitempty"`
-	CreatorCompanyName           string `json:"CreatorCompanyName,omitempty"`
-	CreatorCompanyPath           string `json:"CreatorCompanyPath,omitempty"`
-	CreatorCurrentLanguage       string `json:"CreatorCurrentLanguage,omitempty"`
-	CreatorEmail                 string `json:"CreatorEmail,omitempty"`
-	CreatorIsArchive             bool   `json:"CreatorIsArchive,omitempty"`
-	CreatorLogin                 string `json:"CreatorLogin,omitempty"`
-	CreatorMobilePhone           string `json:"CreatorMobilePhone,omitempty"`
-	CreatorPhone                 string `json:"CreatorPhone,omitempty"`
-	CreatorPosition              string `json:"CreatorPosition,omitempty"`
-	CreatorRoleID                int    `json:"CreatorRoleId,omitempty"`
-	CreatorTimeZone              string `json:"CreatorTimeZone,omitempty"`
-	PriorityDescription          string `json:"PriorityDescription,omitempty"`
-	PriorityImage16Url           string `json:"PriorityImage16Url,omitempty"`
-	PriorityName                 string `json:"PriorityName,omitempty"`
-	ServiceCode                  string `json:"ServiceCode,omitempty"`
-	ServiceDescription           string `json:"ServiceDescription,omitempty"`
-	ServiceIsArchive             bool   `json:"ServiceIsArchive,omitempty"`
-	ServiceIsPublic              bool   `json:"ServiceIsPublic,omitempty"`
-	ServiceName                  string `json:"ServiceName,omitempty"`
-	ServiceParentID              int    `json:"ServiceParentId,omitempty"`
-	ServicePath                  string `json:"ServicePath,omitempty"`
-	StatusName                   string `json:"StatusName,omitempty"`
-	StatusDescription            string `json:"StatusDescription,omitempty"`
-	StatusImage16Url             string `json:"StatusImage16Url,omitempty"`
-	StatusImage24Url             string `json:"StatusImage24Url,omitempty"`
-	StatusIsCommentRequired      bool   `json:"StatusIsCommentRequired,omitempty"`
-	StatusIsFixed                bool   `json:"StatusIsFixed,omitempty"`
-	StatusIsFinal                bool   `json:"StatusIsFinal,omitempty"`
+	CustomFields
+	CompletionStatus           int    `json:"CompletionStatus,omitempty"`
+	WorkflowID                 int    `json:"WorkflowId,omitempty"`
+	IsClient                   bool   `json:"IsClient,omitempty"`
+	FieryFields                string `json:"FieryFields,omitempty"`
+	IsConcurrence              bool   `json:"IsConcurrence,omitempty"`
+	IsCoordinatedByCurrentUser bool   `json:"IsCoordinatedByCurrentUser,omitempty"`
+	CreatorADGuid              string `json:"CreatorADGuid,omitempty"`
+	CreatorComments            string `json:"CreatorComments,omitempty"`
+	CreatorCompanyID           int    `json:"CreatorCompanyId,omitempty"`
+	CreatorCompanyName         string `json:"CreatorCompanyName,omitempty"`
+	CreatorCompanyPath         string `json:"CreatorCompanyPath,omitempty"`
+	CreatorCurrentLanguage     string `json:"CreatorCurrentLanguage,omitempty"`
+	CreatorEmail               string `json:"CreatorEmail,omitempty"`
+	CreatorIsArchive           bool   `json:"CreatorIsArchive,omitempty"`
+	CreatorLogin               string `json:"CreatorLogin,omitempty"`
+	CreatorMobilePhone         string `json:"CreatorMobilePhone,omitempty"`
+	CreatorPhone               string `json:"CreatorPhone,omitempty"`
+	CreatorPosition            string `json:"CreatorPosition,omitempty"`
+	CreatorRoleID              int    `json:"CreatorRoleId,omitempty"`
+	CreatorTimeZone            string `json:"CreatorTimeZone,omitempty"`
+	PriorityDescription        string `json:"PriorityDescription,omitempty"`
+	PriorityImage16Url         string `json:"PriorityImage16Url,omitempty"`
+	PriorityName               string `json:"PriorityName,omitempty"`
+	ServiceCode                string `json:"ServiceCode,omitempty"`
+	ServiceDescription         string `json:"ServiceDescription,omitempty"`
+	ServiceIsArchive           bool   `json:"ServiceIsArchive,omitempty"`
+	ServiceIsPublic            bool   `json:"ServiceIsPublic,omitempty"`
+	ServiceName                string `json:"ServiceName,omitempty"`
+	ServiceParentID            int    `json:"ServiceParentId,omitempty"`
+	ServicePath                string `json:"ServicePath,omitempty"`
+	StatusName                 string `json:"StatusName,omitempty"`
+	StatusDescription          string `json:"StatusDescription,omitempty"`
+	StatusImage16Url           string `json:"StatusImage16Url,omitempty"`
+	StatusImage24Url           string `json:"StatusImage24Url,omitempty"`
+	StatusIsCommentRequired    bool   `json:"StatusIsCommentRequired,omitempty"`
+	StatusIsFixed              bool   `json:"StatusIsFixed,omitempty"`
+	StatusIsFinal              bool   `json:"StatusIsFinal,omitempty"`
 }
 
-// List returns the tasks, selected by parameters
-func (service *TaskService) List(params *TaskListParams) ([]Task, *http.Response, error) {
-	tasks := new([]Task)
+//TasksResponse - json respond of resource list request
+type TasksResponse struct {
+	Tasks []Task `json:"Tasks,omitempty"`
+}
+
+//TaskResponse - json respond of resource request
+type TaskResponse struct {
+	Task Task `json:"Tasks,omitempty"`
+}
+
+// List  - returns the tasks, selected by parameters
+func (service *TaskService) List(params *TaskListParams) (TasksResponse, *http.Response, error) {
+	respond := new(TasksResponse)
 	apiError := new(APIError)
-	resp, err := service.sling.New().Path("issues").QueryStruct(params).Receive(tasks, apiError)
+	resp, err := service.sling.New().Get("task").QueryStruct(params).Receive(respond, apiError)
 	if err == nil {
 		err = apiError
 	}
-	return *tasks, resp, err
+	return *respond, resp, err
+}
+
+// Get  - returns the current tasks, selected by parameters
+func (service *TaskService) Get(ID int) (TaskResponse, *http.Response, error) {
+	respond := new(TaskResponse)
+	apiError := new(APIError)
+	path := fmt.Sprintf("task/%d", ID)
+	resp, err := service.sling.New().Get(path).Receive(respond, apiError)
+	if err == nil {
+		err = apiError
+	}
+	return *respond, resp, err
+}
+
+//AttrParams - parameters for Get Changed Attribut command
+type AttrParams struct {
+	Field         int `url:"field"`
+	Oldworkflowid int `url:"oldworkflowid"`
+	Serviced      int `url:"serviced"`
+	Priorityid    int `url:"priorityid"`
+	Statusid      int `url:"statusid"`
+	Taskid        int `url:"taskid, omitempty"`
+	Creatorid     int `url:"creatorid, omitempty"`
+	Tasktypeid    int `url:"tasktypeid, omitempty"`
+	Assetids      int `url:"assetids, omitempty"`
+	Categoryids   int `url:"categoryids, omitempty"`
+}
+
+//AttrResponse - Attr command response
+type AttrResponse struct {
+	Deadline     string   `json:"Deadline"`
+	ReactionDate string   `json:"ReactionDate"`
+	StatusID     int      `json:"StatusId"`
+	Statuses     []string `json:"Statuses"`
+	WorkflowID   int      `json:"WorkflowId"`
+}
+
+// Attr  - returns the attributes of task with changes
+func (service *TaskService) Attr(params *AttrParams) (AttrResponse, *http.Response, error) {
+	respond := new(AttrResponse)
+	apiError := new(APIError)
+	resp, err := service.sling.New().Get("task").Receive(respond, apiError)
+	if err == nil {
+		err = apiError
+	}
+	return *respond, resp, err
+}
+
+//CreateTaskParams - paramters for Create Task command
+type CreateTaskParams struct {
+	Name             string `url:"Name"`
+	ServiceID        int    `url:"ServiceId"`
+	StatusID         int    `url:"StatusId"`
+	Comment          string `url:"Comment"`
+	Deadline         string `url:"Deadline"`
+	Description      string `url:"Description"`
+	ParentID         int    `url:"ParentId"`
+	PriorityID       int    `url:"PriorityId"`
+	TypeID           int    `url:"TypeId"`
+	CreatorID        int    `url:"CreatorId"`
+	ServiceStageID   int    `url:"ServiceStageId"`
+	IsPrivateComment bool   `url:"IsPrivateComment"`
+	IsMassIncident   bool   `url:"IsMassIncident"`
+	CompletionStatus string `url:"CompletionStatus"`
+	AssetIds         string `url:"AssetIds"`
+	CategoryIds      string `url:"CategoryIdsstring"`
+	ExecutorIds      string `url:"ExecutorIds"`
+	CoordinatorIds   string `url:"CoordinatorIds"`
+	ExecutorGroupID  int    `url:"ExecutorGroupId"`
+	ObserverIds      string `url:"ObserverIds"`
+	FileTokens       string `url:"FileTokens"`
+	DeletedFiles     string `url:"DeletedFiles"`
+	CustomFields
+}
+
+// Create  - returns the attributes of task with changes
+func (service *TaskService) Create(params *CreateTaskParams) (TaskResponse, *http.Response, error) {
+	respond := new(TaskResponse)
+	apiError := new(APIError)
+	resp, err := service.sling.New().Post("task").Receive(respond, apiError)
+	if err == nil {
+		err = apiError
+	}
+	return *respond, resp, err
 }
 
 // Client to wrap services
